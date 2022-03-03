7921d6bd4498c3ab91908fd32586d709963824c1
Machatronics <berkaysarac93@gmail.com>
2022-03-03 16:14:56 +0100

    Continue

--- snake.cpp
+++ snake.cpp
@@ -8,7 +8,8 @@ const int height = 20;
 int x,y, fruitX,fruitY,score;
 enum eDirection{STOP = 0 , LEFT,RIGHT,UP,DOWN};
 eDirection dir;
-
+int tailX[324],int tailY[324];
+int nTail;
 void Setup()
 {
     gameOver= false;
@@ -20,7 +21,7 @@ void Setup()
     score = 0;
 }
 
-void Draw()
+void Draw() 
 {
     system("cls");
     for (int i=0;i < width ; i++)
@@ -39,8 +40,20 @@ void Draw()
             else if (i == fruitY && j == fruitX)
                 cout << "F";
             else
+            {
+                bool print = false; 
+                for(int k= 0 ; k <nTail; k++)
+                {
+                    if(tailX[k] == j && tailY[k]== x )
+                    {
+                        cout << "o"; 
+                        print = true;
+                    }  
+                }
+                if(!print)
                 cout << " ";
 
+            }   
                 if(j== width-1)
                 cout << "#" ;
         }
@@ -50,6 +63,8 @@ void Draw()
             for ( int i =0; i< width; i++)
         cout << "#";
         cout << endl;
+
+        cout << "Score:"  << score << endl;
 }
 
 void Input()
@@ -79,6 +94,21 @@ void Input()
 }
 void Logic()
 {
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X,prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for(int i = 1 ; i < nTail ; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;  
+    }
+
     switch(dir) 
     {
         case LEFT:
@@ -93,7 +123,7 @@ void Logic()
             x++;
             break;
         case UP:
-            if(y==1)
+            if(y==0)
             break;
             y--;
             break;
@@ -104,16 +134,21 @@ void Logic()
             break;
         default:
             break;
-        if(x==fruitX && y== fruitY)
+    for (int i = 0; i< nTail;i++)
+    {
+        if(tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+    }
+
+
+    }
+            if(x==fruitX && y== fruitY)
         {
             score +=10;
             fruitX = rand() % width;
             fruitY = rand() % height;
+            nTail++;
         }
-cout << endl;
-cout << "Score:"  << score << endl;
-
-    }
 }
 
 int main()
@@ -127,3 +162,4 @@ int main()
 
     }
 }
+
